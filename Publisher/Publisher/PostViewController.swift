2db7b64439d70c0998246c9c2085ae6a3fb91413
Prince <princekili0422@gmail.com>
2020-11-20 11:26:09 +0800

    Add HomeVC & PostVC layouts in the storyboard

---
+++ Publisher/Publisher/PostViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PostViewController.swift
+//  Publisher
+//
+//  Created by prince on 2020/11/20.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    @IBOutlet weak var backgroundView: UIView! {
+        
+        didSet {
+            
+            backgroundView.layer.cornerRadius = 5
+        }
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var categoryTextField: UITextField! {
+        
+        didSet {
+            
+            let picker = UIPickerView()
+            picker.delegate = self
+            picker.dataSource = self
+            categoryTextField.inputView = picker
+            picker.tag = 0
+        }
+    }
+    
+    @IBOutlet weak var contentTextView: UITextView! {
+        
+        didSet {
+            
+//            contentTextView.layer.borderWidth = 0.5
+//            contentTextView.layer.borderColor = UIColor.lightGray.cgColor
+            contentTextView.layer.cornerRadius = 5
+            contentTextView.layer.masksToBounds = true
+        }
+    }
+    
+    @IBOutlet weak var postButton: UIButton! {
+        
+        didSet {
+            
+            postButton.layer.cornerRadius = 5
+        }
+    }
+    
+    var categoryArray = ["", "Beauty", "Gossiping", "IU"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func postButtonDidTap(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
+// MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+
+extension PostViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { categoryArray.count }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? { categoryArray[row] }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { categoryTextField.text = categoryArray[row] }
+}
